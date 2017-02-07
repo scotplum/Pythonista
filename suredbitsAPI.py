6357c3c89cc548932eb81336473ecaf6b6509512
Scott Plumbtree <scotplum@gmail.com>
2017-02-07 00:55:03 +0000

    Updated suredbitsAPI.py (added NFL)

--- suredbitsAPI.py
+++ suredbitsAPI.py
@@ -1,22 +1,20 @@
+#Call SuredBits API, retrieve keys and data for today's NBA/NFL games
+#
 import requests
 
 nbagamestodayurl = 'http://api.suredbits.com/nba/v0/games'
 nbagamestoday = requests.get(nbagamestodayurl).json()
+nflgamestodayurl = 'http://api.suredbits.com/nfl/v0/games'
+nflgamestoday = requests.get(nflgamestodayurl).json()
 
 nbagamestodaydict = {}
 nbagamestodaydict = nbagamestoday[0]
-
-#for di in nbagamestoday:
-#    nbagamestodaydict[di['year']]={}
-#    for k in di.keys():
-#        if k =='year': continue
-#        nbagamestodaydict[di['year']][k]=di[k]
-
-
-
 nbagamestoday_keys = nbagamestodaydict.keys()
 
-print(nbagamestoday_keys)
+nflgamestodaydict = {}
+nflgamestodaydict = nflgamestoday[0]
+nflgamestoday_keys = nflgamestodaydict.keys()
 
-#print(nbagamestoday)
-#print(nbagamestoday_keys)
+
+print(nbagamestoday_keys)
+print(nflgamestoday_keys)
