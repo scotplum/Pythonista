19baefeb372e92163469561a4798fa33ec0395b4
Scott Plumbtree <scotplum@gmail.com>
2017-02-16 21:34:52 +0000

    Adding rssaggregatorAPI.py and updating rssAPI.py

--- rssAPI.py
+++ rssAPI.py
@@ -2,7 +2,34 @@ import feedparser
 
 url = 'http://feeds.feedburner.com/TheWirecutter'
 url_parsed = feedparser.parse(url)
+url_entries = url_parsed.entries
+url_entries_selection = url_entries[0:5]
+#url_titles = url_parsed.entries.title
+
 
 feed_title = url_parsed.feed.title
 feed_description =  url_parsed.feed.description
 item_0_title = url_parsed.entries[0].title
+
+rsstitles = []
+rsslinks = []
+
+#Select all titles and add to rsstitles list
+for i in url_entries_selection:
+	rsstitles.append(i['title'])
+
+#Select all links and add to rsslinks
+for i in url_entries_selection:
+	rsslinks.append(i['link'])
+
+print(feed_title)
+#Print Titles: Links
+for i in url_entries_selection:
+	print("[" + i['title'] + "] (" + i['link'] + ")")
+
+#example of selecting a range of titles and assigning to list name
+#for i in url_entries[0:3]:
+#    rssrangetitles.append(i['title']) 
+
+
+
---
+++ rssaggregatorAPI.py
@@ -0,0 +1,38 @@
+import feedparser
+ 
+# Function to fetch the rss feed and return the parsed RSS
+def parseRSS( rss_url ):
+    return feedparser.parse( rss_url ) 
+    
+# Function grabs the rss feed headlines (titles) and returns them as a list
+def getHeadlines( rss_url ):
+    headlines = []
+    
+    feed = parseRSS( rss_url )
+    for newsitem in feed['items']:
+        headlines.append(newsitem['title'])
+    
+    return headlines
+ 
+# A list to hold all headlines
+allheadlines = []
+ 
+# List of RSS feeds that we will fetch and combine
+newsurls = {
+    'apnews':           'http://hosted2.ap.org/atom/APDEFAULT/3d281c11a76b4ad082fe88aa0db04909',
+    'googlenews':       'http://news.google.com/?output=rss',
+    'yahoonews':        'http://news.yahoo.com/rss/'
+}
+ 
+# Iterate over the feed urls
+for key,url in newsurls.items():
+    # Call getHeadlines() and combine the returned headlines with allheadlines
+    allheadlines.extend( getHeadlines( url ) )
+ 
+ 
+# Iterate over the allheadlines list and print each headline
+for hl in allheadlines:
+    print(hl)
+ 
+ 
+# end of code 
